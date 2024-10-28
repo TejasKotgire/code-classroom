6aaae5b2a71ba10da9e07de189f0f10dbb8832e7
Tejas Kotgire <65445982+TejasKotgire@users.noreply.github.com>
2024-10-28 15:29:42 +0530

    Delete backend directory

--- backend/routes/admin_auth.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router();
-
-router.get('/admin/singup', (req, res)=>{
-    res.send("admin signup");
-})
-
-export default router;
--- backend/routes/user_auth.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router();
-
-router.get('/user/signup', (req, res)=>{
-    res.send("user signup");
-})
-
-export default router;
