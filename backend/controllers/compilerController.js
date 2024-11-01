84d22cfc417973995ef9c61c1175df4dab412a3b
Tejas Kotgire <tejaskotgire@gmail.com>
2024-11-01 14:31:01 +0530

    update compiler bacckend

--- backend/controllers/compilerController.js
+++ backend/controllers/compilerController.js
@@ -1,92 +1,45 @@
 var compiler = require("compilex");
-var options = {stats : true};
+var options = { stats: true };
 compiler.init(options);
 
-exports.compiler = async(req, res)=>{
+exports.compiler = async (req, res) => {
     let code = req.body.code;
-    // console.log(code)
     let input = req.body.input;
-    let inputRadio = req.body.inputRadio; //made diff here
-    let lang = req.body.language; //made diff here
-    // console.log(code, input, inputRadio, lang)
+    let inputRadio = req.body.inputRadio;
+    let lang = req.body.language;
 
-    if(lang==="cpp"){
-        if(inputRadio == true){
-            var envData = { OS : "windows" , cmd : "g++", options: {timeout:10000}};
-            compiler.compileCPPWithInput(envData , code , input , function (data) {
-                if(data.error){
-                    // console.log(data.error)
-                    res.status(400).send(data.error)
+    const handleResponse = (data) => {
+        if (data.error) {
+            return res.status(400).send(data.error);
+        }
+        return res.send({ data: data.output });
+    };
 
-                }
-                else{
-                    res.send({data : data.output});
-                }
-            });
+    if (lang === "cpp") {
+        const envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
+        
+        if (inputRadio === true) {
+            compiler.compileCPPWithInput(envData, code, input, (data) => handleResponse(data));
+        } else {
+            compiler.compileCPP(envData, code, (data) => handleResponse(data));
         }
-        else{
-            var envData = { OS : "windows" , cmd : "g++", options: {timeout:10000}};
-            compiler.compileCPP(envData , code , function (data) {
-                if(data.error){
-                    res.status(400).send(data.error)
-                }
-                else{
-                    console.log(data)
-                    res.send({data : data.output});
-                }
-            });
+    } else if (lang === "python") {
+        const envData = { OS: "windows" };
+        
+        if (inputRadio === true) {
+            compiler.compilePythonWithInput(envData, code, input, (data) => handleResponse(data));
+        } else {
+            compiler.compilePython(envData, code, (data) => handleResponse(data));
         }
-    }
-
-    if(lang==="python"){
-        // console.log("kjn")
-        if(inputRadio == true){
-            var envData = { OS : "windows"};
-            compiler.compilePythonWithInput(envData , code , input , function (data) {
-                if(data.error){
-                    res.status(400).send(data.error)
-                }
-                else{
-                    res.send({data : data.output});
-                }
-            });
+    } else if (lang === "java") {
+        const envData = { OS: "windows" };
+        
+        if (inputRadio === true) {
+            compiler.compileJavaWithInput(envData, code, input, (data) => handleResponse(data));
+        } else {
+            compiler.compileJava(envData, code, (data) => handleResponse(data));
         }
-        else{
-            var envData = { OS : "windows"};
-            compiler.compilePython(envData , code , function (data) {
-                if(data.error){
-                    res.status(400).send(data.error)
-                }
-                else{
-                    // console.log("data: ", data.output)
-                    res.send({data : data.output});
-                }
-            });
-        }
-    }
-
-    if(lang === "java"){
-        if(inputRadio == true){
-            var envData = { OS : "windows"};
-            compiler.compileJavaWithInput(envData , code , input , function (data) {
-                if(data.error){
-                    res.status(400).send(data.error)
-                }
-                else{
-                    res.send({data : data.output});
-                }
-            });
-        }
-        else{
-            var envData = { OS : "windows"};
-            compiler.compileJava(envData , code , function (data) {
-                if(data.error){
-                    res.status(400).send(data.error)
-                }
-                else{
-                    res.send({data : data.output});
-                }
-            });
-        }
+    } else {
+        return res.status(400).send("Unsupported language");
     }
 };
